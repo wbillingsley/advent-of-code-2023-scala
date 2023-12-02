873d71aa1e088083250745102f20cb561198a246
Will Billingsley <wbillingsley@cantab.net>
2023-12-02 23:20:57 +1100

    star 2

--- solver.scala
+++ solver.scala
@@ -1,10 +1,29 @@
 import scala.io.*
 
+val numbers = (for 
+    (text, i) <- Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine").zipWithIndex
+yield text -> (i + 1)).toMap
+
+val longestNum = numbers.keySet.map(_.length).max
+
+// Pad the string to ensure a sliding window won't stop early
+val padding = " " * longestNum
+
+def digitify(s:String):Option[Int] = 
+    if s.isEmpty then None
+    else if s(0).isDigit then Some(s(0).asDigit)
+    else for 
+        n <- numbers.keySet.find(s.startsWith(_))
+    yield numbers(n)
+
 def value(s:String):Int = 
     println(s)
-    val digits = s.filter(_.isDigit)
+    val digits = (for 
+        window <- (s + padding).sliding(longestNum) 
+        d <- digitify(window)
+    yield d).toSeq
     println(digits)
-    val d = digits.head.asDigit * 10 + digits.last.asDigit
+    val d = digits.head * 10 + digits.last
     println(d)
     d
 
