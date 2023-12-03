5e8cb39e1b54b86d3db5cc741e6e93bf3eb42fb5
Will Billingsley <wbillingsley@cantab.net>
2023-12-03 17:07:44 +1100

    Star 6

--- solver.scala
+++ solver.scala
@@ -24,6 +24,13 @@ def symbolCoordinates(lines:Seq[String]):Seq[Coord] =
         (ch, x) <- line.zipWithIndex if !ch.isDigit && !ch.isLetter && (!". ".contains(ch)) 
     yield (x, y)   
 
+// Just the asterisks. We only do this twice, so let's not shrink the code duplication yet
+def asteriskCoordinates(lines:Seq[String]):Seq[Coord] = 
+    for 
+        (line, y) <- lines.zipWithIndex
+        (ch, x) <- line.zipWithIndex if ch == '*' 
+    yield (x, y)   
+
 // I'm not a big fan of MatchIterator not being an Iterator of matches (it's an iterator of Strings) but oh well
 def numbersInLine(y:Int, line:String):Seq[PartNumber] = 
     val found = scala.collection.mutable.Buffer.empty[PartNumber]
@@ -40,19 +47,26 @@ def numbersInLine(y:Int, line:String):Seq[PartNumber] =
 @main def main() = 
     val lines = Source.fromFile("input.txt").getLines().toSeq
     val symbolCoords = symbolCoordinates(lines)
-    println(symbolCoords)
-
-    println("""
-
-    ---
+    val astCoords = asteriskCoordinates(lines)
     
-    """)
     val partNumbers = for 
         (line, y) <- lines.zipWithIndex 
         (pn, range) <- numbersInLine(y, line.trim()) if symbolCoords.exists((c) => isAdjacent(c, range))
-    yield pn
-    println(partNumbers)
-    println(partNumbers.length)
-    val sum = partNumbers.sum
-    println(s"Sum of part numbers is ${sum}")
+    yield (pn, range)
+
+    // a gear is an asterisk adjacent to two partNumbers
+    val gears = 
+        for asterisk <- astCoords yield
+            val pns = for 
+                (pn, range) <- partNumbers if isAdjacent(asterisk, range)
+            yield (pn, range)
+
+            if pns.length == 2 then 
+                val (a, _) = pns(0)
+                val (b, _) = pns(1)
+                a * b
+            else 0
+
+    println(gears)
+    println(s"Sum of gears is ${gears.sum}")
 
