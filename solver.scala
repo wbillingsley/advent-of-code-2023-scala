cb8f3b9f7414d2ac68ebfe6ea188aeb82bbb106a
William Billingsley <wbillingsley@cantab.net>
2023-12-05 17:10:18 +1100

    forgot a line of instruction, dead end
    Well isn't that annoying -- I forgot the line about unmapped mappings and tried to solve a much harder problem than the real one! Oops

--- solver.scala
+++ solver.scala
@@ -25,13 +25,22 @@ case class Mapping(sourceStart:Long, destStart:Long, length:Long) {
 
     def dest(source:Long):Long = 
         source - sourceStart + destStart
+
+    def get(n:Long):Option[Long] = 
+        if sourceIncludes(n) then Some(dest(n)) else None
 }
 
-class Mapper(from:String, to:String) {
+class Mapper(val from:String, val to:String) {
     private val mappings = mutable.Buffer.empty[Mapping]
 
     def addMapping(m:Mapping):Unit = 
         mappings.append(m)
+
+    def sourceContains(n:Long):Boolean =
+        mappings.exists((m) => m.sourceIncludes(n))
+
+    def get(n:Long):Option[Long] = 
+        mappings.find(_.sourceIncludes(n)).map(_.dest(n))
 
 
 }
@@ -39,11 +48,14 @@ class Mapper(from:String, to:String) {
 @main def main() = 
     val lines = Source.fromFile("input.txt").getLines().toSeq
 
-    // let's read this one in mutably
+    // let's read this one in mutably.
 
     var seeds = Seq.empty[Long]
     var maps = mutable.Map.empty[(String, String), Mapper]
     var currentMapper:Option[Mapper] = None
+
+    var accumulateNouns = Set.empty[String]
+    def nouns = accumulateNouns.toSeq
 
     lines.foreach {
         case seedsRegex(numbers) => 
@@ -51,6 +63,7 @@ class Mapper(from:String, to:String) {
             println(s"Seeds are $seeds")
         case mapRegex(from, to) => 
             println(s"Found map from $from to $to")
+            accumulateNouns = accumulateNouns ++ Set(from, to)
             val m = Mapper(from, to)
             maps((from, to)) = m
             currentMapper = Some(m)
@@ -60,10 +73,53 @@ class Mapper(from:String, to:String) {
             val Array(destStart, sourceStart, length) = s.split(' ').map(_.trim).map(_.toLong)
             for m <- currentMapper do
                 val mapping = Mapping(sourceStart, destStart, length)
-                println(mapping)
                 m.addMapping(mapping)
 
     } 
+
+    // Let's do this the sneaky mutable way.
+    // ensure there is a mapping from everything to everything
+    for 
+        from <- nouns
+        to <- nouns if to != from && !maps.contains((from, to))
+    do
+        maps((from, to)) = Mapper(from, to)
+
+
+    // Given a mapping, updates everything else it can possibly know
+    def transitivelyUpdateMapping(m:Mapper, source:Long, dest:Long):Unit = {
+        //if !m.sourceContains(source) then
+            m.addMapping(Mapping(source, dest, 1))
+
+            println()
+
+            for 
+                transitiveMap <- maps.values.filter(_.from == m.to)
+                inferredDest <- transitiveMap.get(dest)
+            do
+                println(s"Also transitively mapping from ${m.from} $source to ${transitiveMap.to} $inferredDest")
+                transitivelyUpdateMapping(maps((m.from, transitiveMap.to)), source, inferredDest)
+    }
+
+    val seedMaps:Map[String, Mapper] = (for n <- nouns.toSeq if n != "seed" yield
+        val map = maps.getOrElse(("seed", n), Mapper("seed", n))
+        n -> map).toMap
+
+    val locationMap = seedMaps("location")
+
+
+    for s <- seeds do 
+        for 
+            m <- seedMaps.values 
+            d <- m.get(s)
+        do 
+            println(s"Found seed map from $s to ${m.to} $d")
+            // hacky 
+            transitivelyUpdateMapping(m, s, d)
+
+    //for s <- seeds do
+    println("Missing:" + seeds.filter((s) => locationMap.get(s).isEmpty)) 
+
 
     
 
