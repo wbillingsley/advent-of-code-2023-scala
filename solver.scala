b51a3019c820d46282846f185fc04357d5c6d221
William Billingsley <wbillingsley@cantab.net>
2023-12-05 16:17:10 +1100

    reads stuff in for star 9
    still working on solver though

--- solver.scala
+++ solver.scala
@@ -2,59 +2,45 @@ import scala.io.*
 import scala.collection.immutable.Queue
 import scala.annotation.tailrec
 
-// This is the solution for part 2
-// For the solution to part 1, https://github.com/wbillingsley/advent-of-code-2023-scala/blob/star7/solver.scala
-// (or select the "star7" branch from GitHub)
+// This is the solution for part 1
+// For the solution to part 2, https://github.com/wbillingsley/advent-of-code-2023-scala/blob/star9/solver.scala
+// (or select the "star9" branch from GitHub)
 
 // A regex to capture numbers in lines
 val number = raw"(\d+)".r
 val card = raw"Card\s+(\d+): (.*)".r
 
-// I still had this left from yesterday, so let's just modify it
-def numbersInText(line:String):Seq[Int] = 
-    number.findAllIn(line).map(_.toInt).toSeq
+val seedsRegex = raw"seeds: (.*)".r
+val mapRegex = raw"(\w+)-to-(\w+) map:".r
 
-case class Card(num:Int, winning:Seq[Int], have:Seq[Int])(cards: => Seq[Card]) {
-
-    def matching = have.count(winning.contains)
-
-    //No such thing as score any more
-    //lazy val score:Int = if matching > 0 then 
-    //    Math.pow(2, matching - 1).toInt
-    //else 0
-
-    // Let's keep track of which cards we win from this card
-    lazy val wins:Seq[Int] = Range(num + 1, num + 1 + matching).toSeq
-
-    // This is a little like Fibonacci -- the value of the next one in the sequence depends on the previous one.
-    // We're going to need some memoization, but we can use a lazy val in each card instead of fold
-    lazy val countOfThis:Int =
-        1 + (for n <- (1 until num) if cards(n).wins.contains(num) yield cards(n).countOfThis).sum
-
-}
-
-def lineToCard(s:String)(getCards: => Seq[Card]):Card = {
-    s match {
-        case card(id, text) => 
-            val Seq(winningText, haveText) = text.split('|').toSeq
-            val winning = numbersInText(winningText)
-            val have = numbersInText(haveText)
-            Card(id.toInt, winning, have)(getCards)
-
-        case x => 
-            println(s"Unmatched $x")
-            throw new IllegalArgumentException(".")
-    }
-}
+// We'll use this to read the data in. Then we'll work with it.
+import scala.collection.mutable
 
 @main def main() = 
     val lines = Source.fromFile("input.txt").getLines().toSeq
-    lazy val cards:Seq[Card] = Card(0, Nil, Nil)(cards) +: lines.map(lineToCard(_)(cards))
-    // for c <- cards if c.num >= 1 do
-    //     println(s"Card ${c.num} occurs ${c.countOfThis} times and wins ${c.wins}")
-    
 
-    //val won = cardCollector(cardMap, Nil, Queue(cards*))
-    println(s"Won ${cards.tail.map(_.countOfThis).sum} cards")
+    // let's read this one in mutably
+
+    var seeds = Seq.empty[Long]
+    var maps = mutable.Map.empty[(String, String), mutable.Map[Long, Long]]
+    var currentMap:Option[mutable.Map[Long, Long]] = None
+
+    lines.foreach {
+        case seedsRegex(numbers) => 
+            seeds = numbers.trim.split(' ').map(_.toLong)
+            println(s"Seeds are $seeds")
+        case mapRegex(from, to) => 
+            println(s"Found map from $from to $to")
+            val m = mutable.Map.empty[Long, Long]
+            maps((from, to)) = m
+            currentMap = Some(m)
+        case s if s.isBlank() => 
+            ()
+        case s => 
+            val numbers = s.split(' ').map(_.trim).map(_.toLong).toSeq
+            println(numbers)
+
+    } 
+
     
 
