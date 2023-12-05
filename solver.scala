61d46fbafd4ac14e21074341c1478185c19bc548
William Billingsley <wbillingsley@cantab.net>
2023-12-05 20:27:03 +1100

    buggy but in progress
    Mapper.transform has a bug in it when producing the seed to humidity map on the test input

--- solver.scala
+++ solver.scala
@@ -32,9 +32,6 @@ class LongRange(s:Long, e:Long) {
 
     def contains(l:Long):Boolean = l >= start && l < exclEnd
 
-    def contains(r:LongRange):Boolean = 
-        contains(r.start) && r.exclEnd > start && r.exclEnd <= exclEnd
-
     def intersects(r:LongRange):Boolean = 
         Math.max(start, r.start) < Math.min(exclEnd, r.exclEnd)
 
@@ -106,20 +103,33 @@ class Mapper(val from:String, val to:String) {
     def addMapping(m:Mapping):Unit = 
         _mappings.append(m)
 
-    lazy val initialMappings = _mappings.toSeq.sortBy(_.from.start)
+    def mappings = _mappings.toSeq.sortBy(_.from.start)
 
-    lazy val mappings = 
+    def apply(n:Long):Option[Long] = 
+        mappings.find(_.contains(n)).map(_.apply(n))
+
+    def fillBlanks():Unit = 
+        println(s"Filling in the blanks for $from to $to")
+
         var last = 0L
-        (for 
-            i <- initialMappings
+        val fill = (for 
+            i <- mappings
             m <- {
-                if i.from.start == last then Seq(i)
-                else Seq(Mapping(LongRange(last, i.from.start), 0), i)
+                println(s"found mapping $i; last is $last")
+                if i.from.start == last then 
+                    last = i.from.exclEnd
+                    Seq.empty
+                else
+                    println(s"filling a blank from $last to ${i.from.start}") 
+                    last = i.from.exclEnd
+                    Seq(Mapping(LongRange(last, i.from.start), 0))
             }
-        yield 
-            last = i.from.exclEnd
-            m
-        ) :+ Mapping(LongRange(last, Long.MaxValue), 0)
+        yield m) :+ Mapping(LongRange(last, Long.MaxValue), 0)
+
+        for m <- fill do addMapping(m)
+
+        println(this)
+        println
 
     
     def transform(m:Mapper):Mapper = {
@@ -139,7 +149,7 @@ class Mapper(val from:String, val to:String) {
 
     }
 
-    override def toString():String = s"""|Mapping $from -> $to
+    override def toString():String = s"""|Mapper from $from to $to
                        |""".stripMargin +
                        (for (m <- mappings) yield m.toString + "\n")
 
@@ -147,7 +157,7 @@ class Mapper(val from:String, val to:String) {
 
 
 @main def main() = 
-    val lines = Source.fromFile("test.txt").getLines().toSeq
+    val lines = Source.fromFile("input.txt").getLines().toSeq
 
     // let's read this one in mutably.
 
@@ -169,7 +179,9 @@ class Mapper(val from:String, val to:String) {
             maps((from, to)) = m
             currentMapper = Some(m)
         case s if s.isBlank() => 
-            ()
+            // We've finished the current mapper, so fill in any gaps
+            for m <- currentMapper do m.fillBlanks()
+        
         case s => 
             val Array(destStart, sourceStart, length) = s.split(' ').map(_.trim).map(_.toLong)
             for m <- currentMapper do
@@ -179,16 +191,25 @@ class Mapper(val from:String, val to:String) {
     } 
 
     val identityMapper = Mapper("seed", "seed")
-    identityMapper.addMapping(Mapping(LongRange(0, Long.MaxValue), 0))
+    for Seq(start, range) <- seeds.grouped(2) do
+        identityMapper.addMapping(Mapping(LongRange(start, start + range), 0))
+
+    println(identityMapper)
+
+    println(maps(("temperature", "humidity")))
 
     val composed = mappingSuccession.foldLeft(identityMapper) { case (mapper, (from, to)) =>
         val m = maps((from, to))
-        m.transform(mapper)
+        val composed = m.transform(mapper)
+
+        println(composed)
+        println(composed.apply(82))
+
+        composed
     }
 
 
-    println(composed)
-
+    println(composed.mappings.map(_.to).filter(!_.isEmpty).map(_.start).min)
 
 
 
