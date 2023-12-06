d5629de7a46e8c30fb4fe0e952163f1b4964cd17
William Billingsley <wbilling@une.edu.au>
2023-12-06 16:10:57 +1100

    star11

--- solver.scala
+++ solver.scala
@@ -17,8 +17,18 @@ val distancesRegex = raw"Distance: (.*)".r
 
 import util.* 
 
+
+// How far we travel for a given button push length
+def travelTimes(time:Int) = {
+    for 
+        i <- 0 to time
+    yield 
+        i * (time - i)
+}
+
+
 @main def main() = 
-    val lines = Source.fromFile("test.txt").getLines().toSeq
+    val lines = Source.fromFile("input.txt").getLines().toSeq
 
     // Today's file is short and in a fixed format. Let's just get it in
     val times = lines(0) match {
@@ -30,6 +40,17 @@ import util.*
             number.findAllIn(text).map(_.toInt).toSeq
     }
     println(distances)
+
+    def waysToWin(race:Int):Int = {
+        val d = distances(race)
+        travelTimes(times(race)).filter(_ > d).length
+    }
+
+    println(distances.indices.map(waysToWin).product)
+
+
+
+
 
 
 
