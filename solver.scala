1aae711953c875492e9f7847006452379e82f42d
William Billingsley <wbilling@une.edu.au>
2023-12-06 16:22:50 +1100

    Fix comment link to part 1

--- solver.scala
+++ solver.scala
@@ -2,9 +2,9 @@ import scala.io.*
 import scala.collection.immutable.Queue
 import scala.annotation.tailrec
 
-// This is the solution for part 1
-// For the solution to part 2, https://github.com/wbillingsley/advent-of-code-2023-scala/blob/star9/solver.scala
-// (or select the "star9" branch from GitHub)
+// This is the solution for part 2
+// For the solution to part 1, https://github.com/wbillingsley/advent-of-code-2023-scala/blob/star11/solver.scala
+// (or select the "star11" branch from GitHub)
 
 // A regex to capture numbers in lines
 val number = raw"(\d+)".r
