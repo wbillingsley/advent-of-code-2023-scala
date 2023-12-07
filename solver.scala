1ba6d649ca3fa302c1bcaa523ae5be6706f1118a
William Billingsley <wbilling@une.edu.au>
2023-12-07 16:54:15 +1100

    remove some unused lines
    lots of bits of previous "star"s left behind

--- solver.scala
+++ solver.scala
@@ -1,29 +1,8 @@
-//> using dep org.typelevel::cats-collections-core:0.9.8
-
-
 // This is the solution for part 2
 // For the solution to part 1, https://github.com/wbillingsley/advent-of-code-2023-scala/blob/star13/solver.scala
 // (or select the "star13" branch from GitHub)
 
-
-// just in case, let's keep Diet in scope. There's been lots of range problems.
-//import cats._, cats.implicits._, cats.collections._, cats.collections.syntax.all._
-
 import scala.io.*
-import scala.collection.immutable.Queue
-import scala.annotation.tailrec
-
-
-// A regex to capture numbers in lines
-val number = raw"(\d+)".r
-val card = raw"Card\s+(\d+): (.*)".r
-
-val timesRegex = raw"Time: (.*)".r
-val distancesRegex = raw"Distance: (.*)".r
-
-// val mapRegex = raw"(\w+)-to-(\w+) map:".r
-
-// import util.* 
 
 val cardMap = Seq(
     'A' -> 14, 'K' -> 13, 'Q' -> 12, 'T' -> 10,
