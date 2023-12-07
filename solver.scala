ec1519892c515b9b04d8f875949409af40e796de
William Billingsley <wbilling@une.edu.au>
2023-12-07 16:28:06 +1100

    star 13

--- solver.scala
+++ solver.scala
@@ -9,7 +9,6 @@
 // just in case, let's keep Diet in scope. There's been lots of range problems.
 //import cats._, cats.implicits._, cats.collections._, cats.collections.syntax.all._
 
-
 import scala.io.*
 import scala.collection.immutable.Queue
 import scala.annotation.tailrec
@@ -26,12 +25,51 @@ val distancesRegex = raw"Distance: (.*)".r
 
 // import util.* 
 
+val cardMap = Seq(
+    'A' -> 14, 'K' -> 13, 'Q' -> 12, 'J' -> 11, 'T' -> 10,
+    '9' -> 9, '8' -> 8, '7' -> 7, '6' -> 6, '5' -> 5, '4' -> 4, '3' -> 3, '2' -> 2
+).toMap
+
+enum HandType:
+    case HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind
+
+import HandType.*
+
+
+case class Hand(s:String) {
+
+    val grouplengths = s.groupBy(identity).map((c, s) => (c, s.length))
+
+    lazy val typ:HandType = {
+        if grouplengths.values.max == 5 then FiveOfAKind else 
+        if grouplengths.values.max == 4 then FourOfAKind else
+        if grouplengths.values.toSeq.contains(3) && grouplengths.values.toSeq.contains(2) then FullHouse else
+        if grouplengths.values.toSeq.contains(3) then ThreeOfAKind else
+        if grouplengths.values.count(_ == 2) == 2 then TwoPair else
+        if grouplengths.values.toSeq.contains(2) then OnePair else
+        HighCard
+    }
+
+    def fakeFaceValue = s.foldLeft(0) { case (t, c) => t * 16 + cardMap(c) }
+
+    // A filthy hack!
+    def handValue = (16777216 * typ.ordinal) + fakeFaceValue
+
+}
+
+def decompose(line:String):(Hand, Int) = 
+    val s"$hand $bid" = line
+    (Hand(hand), bid.toInt)
+
 
 @main def main() = 
     val lines = Source.fromFile("input.txt").getLines().toSeq
+    val pairs = lines.map(decompose)
+    val sorted = pairs.sortBy(_._1.handValue)
+    println(sorted.zipWithIndex.map({ case ((h, b), i) => b * (i + 1) }).sum )
 
     // May be useful to have this to spot crashes if using watch
-    println("Re-ran at " + java.util.Date().toLocaleString())
+    println("Re-ran at: " + java.util.Date().toLocaleString())
 
 
 
