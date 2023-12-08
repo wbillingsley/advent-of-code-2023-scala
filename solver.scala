e37b56ac296182603b6a78c219213acce91ffb4f
William Billingsley <wbilling@une.edu.au>
2023-12-08 16:22:55 +1100

    first plan brute force
    If this takes a while, we'll do it more sensibly

--- solver.scala
+++ solver.scala
@@ -20,19 +20,22 @@ val directions = "LR"
         node -> Seq(left, right)).toMap
 
     @tailrec
-    def navigate(from:String, instructions:String, target:String, stepCount:Int = 0):Int = 
-        if from == target then stepCount 
+    def navigate(from:Seq[String], instructions:String, target:String, stepCount:Int = 0):Int = 
+        if from.forall(_.last == target.last) then stepCount 
         else 
             if instructions.isEmpty() then 
+                println(s"Looped with stepCount $stepCount")
+                for s <- from do println(s)
                 navigate(from, instructions0, target, stepCount)
             else
                 val h = instructions.head
                 
                 val dir = directions.indexOf(h)
-                val newNode:String = network(from)(dir)
+                val newNode = from.map((f) => network(f)(dir))
                 navigate(newNode, instructions.drop(1), target, stepCount + 1)
 
-    println(navigate("AAA", instructions0, "ZZZ", 0))
+    val start = network.keySet.toSeq.filter(_.endsWith("A"))
+    println(navigate(start, instructions0, "ZZZ", 0))
 
     
 
