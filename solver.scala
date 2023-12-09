d366d235b7ed8fcb83eae63355af487afca6a100
Will Billingsley <wbillingsley@cantab.net>
2023-12-09 16:47:01 +1100

    star18

--- solver.scala
+++ solver.scala
@@ -11,27 +11,46 @@ type Line = Seq[Int]
 
 val number = raw"([-\d]+)".r
 
-def decompose(line:String):Seq[Int] = 
-    number.findAllIn(line).map(_.toInt).toSeq
+def decompose(line:String):Seq[Long] = 
+    number.findAllIn(line).map(_.toLong).toSeq
 
-def differentiate(s:Seq[Int]):Seq[Int] = 
+def differentiate(s:Seq[Long]):Seq[Long] = 
     s.zip(s.tail).map((a, b) => b - a)
 
 
-def differentiateToZeroes(s:Seq[Int]):(Int, Seq[Seq[Int]]) = 
+def differentiateToZeroes(s:Seq[Long]):Seq[Seq[Long]] = 
     var count = 0
     var cursor = List(s)
-    while !cursor.head.isEmpty && cursor.head.exists(_ != 0) do
+    while cursor.head.exists(_ != 0) do
         count = count + 1
         cursor = differentiate(cursor.head) :: cursor
-    (count, cursor)
-
-def nextValue(diffTable:Seq[Seq[Int]]):Int = {
-    var cursor = 0
-    for i <- diffTable.indices.reverse do
-        cursor = diffTable(i).last + cursor
     cursor
 
+def nextValue(diffTable:Seq[Seq[Long]]):Long = {
+    var cursor = 0L
+    for i <- diffTable.indices.reverse do
+        cursor = diffTable(i).head - cursor
+    cursor
+}
+
+def prependHistory(diffTable:Seq[Seq[Long]]):Seq[Seq[Long]] = {
+    var cursor = 0L
+    println("Last " + diffTable.head)
+    for i <- diffTable.indices yield
+        val line = diffTable(i)
+        cursor = line.head - cursor
+        val r = cursor +: line
+        r
+}
+
+def checkHistory(h:Seq[Seq[Long]]):Boolean = {
+    var error = false
+    for Seq(line, next) <- h.sliding(2) yield
+        for i <- next.indices if next(i) != line(i + 1) - line(i) yield
+            println(h)
+            throw new IllegalStateException(s"${line(i + 1)} - ${line(i)} != $next(i)")
+
+    true
 }
 
 @main def main() = 
@@ -39,13 +58,21 @@ def nextValue(diffTable:Seq[Seq[Int]]):Int = {
     val histories = lines.map(decompose)
     val differentiated = histories.map(differentiateToZeroes)
 
-    val nexts = for (s, table) <- differentiated yield nextValue(table)
+    // val nexts = for (s, table) <- differentiated yield 
+    //     val v = nextValue(table)
+    //     println(v)
+    //     v
     
+    val prepended = differentiated.map(prependHistory)
 
+    // println(prepended(0))
+    // for p <- prepended yield 
+    //     println(p.head)
+    //     checkHistory(p)
 
+    val firsts = for p <- prepended yield p.last.head
 
-
-    println(nexts.sum)
+    println("Total: " + firsts.sum)
 
 
 
