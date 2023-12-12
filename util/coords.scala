359102cb201798816abe69b3ace05354a4c3f8a2
William Billingsley <wbillingsley@cantab.net>
2023-12-12 15:58:10 +1100

    clean up for star 23

---
+++ util/coords.scala
@@ -0,0 +1,18 @@
+package util
+
+type Coord = (Int, Int)
+
+extension (c:Coord) {
+    def +(c2:Coord) = 
+        val (x, y) = c
+        val (xx, yy) = c2
+        (xx + x, yy + y)
+
+    def inverse = (-c._1, -c._2)
+}
+
+val North = (0, -1)
+val South = (0, 1)
+val East = (1, 0)
+val West = (-1, 0)
+val all = Seq(North, South, East, West)
