c6c45f23e4c356ac3803fb25ca5042d76b89d29e
William Billingsley <wbillingsley@cantab.net>
2023-12-14 16:17:24 +1100

    star 27

--- solver.scala
+++ solver.scala
@@ -20,6 +20,49 @@ def allDecimalsIn(s:String) = decimals.findAllIn(s).map(_.toDouble).toSeq
 @main def main() = 
     val lines = Source.fromFile("input.txt").getLines().toSeq
 
+    // Just one grid today
+    val puzzle = lines.map(_.toSeq)
+
+    val transposed = puzzle.transpose
+
+    def roll(section:Seq[Char]):Seq[Char] = 
+        val (os, dots) = section.partition(_ == 'O')
+        os ++ dots
+
+    def rollLine(l:Seq[Char]):Seq[Char] =
+        val buf = mutable.Buffer.empty[Char]
+        var remaining = l
+        while remaining.nonEmpty do 
+            val sec = remaining.takeWhile(_ != '#')
+            val rolled = roll(sec)
+            buf.append(rolled*)
+            if sec.length == remaining.length then
+                remaining = remaining.drop(sec.length)
+            else 
+                buf.append('#')
+                remaining = remaining.drop(sec.length + 1)
+        buf.toSeq
+
+    def loadOnLine(l:Seq[Char]):Int = 
+        val os = l.takeWhile(_ != '#').filter(_ == 'O')
+        val loads = for (o, y) <- os.zipWithIndex yield l.length - y
+        println(s"Calculated rock loads $loads")
+        loads.sum
+
+    val loads = for line <- transposed yield 
+        val rolled = rollLine(line)
+        val loads = for (o, y) <- rolled.zipWithIndex if o == 'O' yield line.length - y
+        loads.sum
+
+
+
+    println(rollLine("OO...O.#..O..O..".toSeq))
+
+    println(loads.sum)
+
+    
+
+    
 
     // May be useful to have this to spot crashes if using watch
     println("Re-ran at: " + java.util.Date().toLocaleString())
