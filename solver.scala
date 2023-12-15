7cb54ef8abc89ac8c3c684ef5c5b123ffe2250cc
William Billingsley <wbillingsley@cantab.net>
2023-12-15 19:05:38 +1100

    fix comment link to other part

--- solver.scala
+++ solver.scala
@@ -1,6 +1,6 @@
-// This is the solution for part 2
-// For the solution to part 1, https://github.com/wbillingsley/advent-of-code-2023-scala/blob/star27/solver.scala
-// (or select the "star27" branch from GitHub)
+// This is the solution for part 1
+// For the solution to part 2, https://github.com/wbillingsley/advent-of-code-2023-scala/blob/star30/solver.scala
+// (or select the "star30" branch from GitHub)
 
 import scala.io.*
 import scala.annotation.tailrec
