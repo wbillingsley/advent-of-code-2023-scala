b7ae5532d976a2658dde6c26e12d2093a0e0ccd8
William Billingsley <wbillingsley@cantab.net>
2023-12-15 19:04:24 +1100

    star 20

--- solver.scala
+++ solver.scala
@@ -7,6 +7,7 @@ import scala.annotation.tailrec
 import scala.collection.mutable
 
 import util.*
+import scala.collection.immutable.Queue
 
 type Line = Seq[Int]
 
@@ -22,12 +23,40 @@ def hashScore(s:String) =
         (t + c) * 17 % 256
     }
 
+case class Label(l:String, n:Int)
+
 @main def main() = 
-    val lines = Source.fromFile("test.txt").getLines().toSeq
+    val lines = Source.fromFile("input.txt").getLines().toSeq
 
     val puz = lines(0)
     val strings = puz.split(',')
-    println("Result is " + strings.map(hashScore).sum)
+    // println("Result is " + strings.map(hashScore).sum)
+
+    var arrangement:Map[Int, Queue[Label]] = (for i <- 0 until 256 yield i -> Queue.empty).toMap
+
+    strings.foreach {
+        case s"$l-" => 
+            val box = hashScore(l)
+            arrangement = arrangement.updated(box, arrangement(box).filter(_.l != l))
+            ()
+        case s"$l=$n" =>
+            val focal = n.toInt
+            val box = hashScore(l)
+            val old = arrangement(box).indexWhere(_.l == l)
+            if old >= 0 then 
+                arrangement = arrangement.updated(box, arrangement(box).updated(old, Label(l, focal)))
+            else 
+                arrangement = arrangement.updated(box, arrangement(box).enqueue(Label(l, focal)))
+
+    }
+
+    val overallScore = (for 
+        (boxNum, q) <- arrangement
+        (lens, order) <- q.zipWithIndex
+    yield (boxNum + 1) * (order + 1) * lens.n).sum
+
+    println("overall " + overallScore)
+
 
 
     // May be useful to have this to spot crashes if using watch
