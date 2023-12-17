93927f7b2ba032e73af5714fa6de60f19b3e2179
William Billingsley <wbillingsley@cantab.net>
2023-12-17 20:46:28 +1100

    star 33
    Started late and have to interrupt to go to carols

--- solver.scala
+++ solver.scala
@@ -17,81 +17,77 @@ val decimals = "(-?\\d+)([.]\\d+)?".r
 def allIntegersIn(s:String) = integers.findAllIn(s).map(_.toLong).toSeq
 def allDecimalsIn(s:String) = decimals.findAllIn(s).map(_.toDouble).toSeq
 
-type Beam = (Coord, Coord)
+// location, heading (where from), step count
+type Step = (Coord, Coord, Int)
+type Path = Seq[Step]
+
+type DistanceMemo = (Step, Int)
+
+// perpendicular to current heading
+def ninetyDegrees(heading:Coord):Seq[Coord] = heading match {
+    case North | South => Seq(East, West)
+    case East | West => Seq(North, South)
+    case _ => Seq(North, East, South, West) // starting case
+}
+
+// allowable moves
+def possibilities(s:Step):Seq[Step] = {
+    val (p, head, c) = s
+    if s._2 == (0,0) then Seq((p + North, North, 1), (p + East, East, 1), (p + South, South, 1), (p + West, West, 1)) // starting case
+    else if c >= 3 then 
+        for d <- ninetyDegrees(head) yield (p + d, d, 1)
+    else (p + head, head, c + 1) +: (for d <- ninetyDegrees(head) yield (p + d, d, 1)) 
+}
+
 
 @main def main() = 
     val lines = Source.fromFile("input.txt").getLines().toSeq
 
-    val puz:Seq[Seq[Char]] = lines.map(_.toSeq)
+    val puz:Seq[Seq[Int]] = lines.map { (l) => 
+        for 
+            c <- l.toSeq
+        yield ("" + c).toInt
+    }
+    val maxY = puz.indices.max
+    val maxX = puz(maxY).indices.max
 
     def puzContains(p:Coord):Boolean = 
         val (x, y) = p
+        // x < 2 && y < 2 &&
         puz.indices.contains(y) && puz(y).indices.contains(x)
 
-    def charAt(coord:Coord):Char = 
+    def distAt(coord:Coord):Int = 
         val (x, y) = coord
         puz(y)(x)
 
-    def traverse(start:(Coord, Coord)):Int =
-        val beams:Seq[Beam] = Seq(start)
-        var energised = Set.empty[Beam]
-
-        var b = beams
-        println("before")
+    val paths:Seq[DistanceMemo] = Seq(((0, 0), (0,0), 0) -> 0)
+    def traverse() = 
+        var b = paths
+        var memo = Map.empty[Step, Int]
         while !b.isEmpty do 
-            b.foreach { (beam) => energised = energised + beam }
-            b = (b.flatMap { (p, dir) => 
-                (charAt(p), dir) match {
-                    case ('.', _) => 
-                        Seq((p + dir, dir))
+            // println(b)
+            //b.foreach { (step, dist) => memo = memo.updated(step, dist) }
+            b = (b.flatMap { (step, dist) => 
 
-                    case ('/', East) => 
-                        Seq((p + North, North))
-                    case ('/', West) => 
-                        Seq((p + South, South))
-                    case ('/', North) => 
-                        Seq((p + East, East))
-                    case ('/', South) => 
-                        Seq((p + West, West))
+                for 
+                    next <- possibilities(step) if puzContains(next._1)                     
+                    nextD = dist + distAt(next._1) if !memo.contains(next) || memo(next) > nextD
+                yield 
+                    memo = memo.updated(next, nextD)
+                    (next, nextD)
 
-                    case ('\\', East) => 
-                        Seq((p + South, South))
-                    case ('\\', West) => 
-                        Seq((p + North, North))
-                    case ('\\', North) => 
-                        Seq((p + West, West))
-                    case ('\\', South) => 
-                        Seq((p + East, East))
+            })
+            println(s"Handling ${b.length} distinct paths")
 
-                    case ('-', East) => Seq(((p + dir), dir))
-                    case ('-', West) => Seq(((p + dir), dir))
-                    case ('|', North) => Seq(((p + dir), dir))
-                    case ('|', South) => Seq(((p + dir), dir))
+        memo
 
-                    case ('-', North) => Seq(((p + East), East), ((p + West), West) )
-                    case ('-', South) => Seq(((p + East), East), ((p + West), West) )
-                    case ('|', East) => Seq(((p + North), North), ((p + South), South))
-                    case ('|', West) => Seq(((p + North), North), ((p + South), South))
-                }
+    println(s"bottom right: $maxX $maxX")
 
-            }).filter((beam) => !energised.contains(beam) && puzContains(beam._1))
-
-        energised.map((beam, dir) => beam).size
-
-
-    val maxY = puz.indices.last
-    val starts = 
-        puz.indices.map((y) => (0, y) -> East) ++ 
-        puz.indices.map((y) => (puz(y).indices.last, y) -> West) ++
-        puz(0).indices.map((x) => (x, 0) -> South) ++ 
-        puz(maxY).indices.map((x) => (x, maxY) -> North) 
-
-
-    println("Check " + traverse((0, 0) -> East))
-
-    val result = starts.map(traverse).max
-    println("result " + result)
-    
+    println("before")
+    val memo = traverse()
+    println(memo.size)
+    val result = memo.filter({ (step, d) => step._1 == (maxX, maxY) }).minBy(_._2)
+    println(s"Result $result")
 
 
 
