1ee9946af7b86879368035cac8e8cbd57fcc4436
William Billingsley <wbillingsley@cantab.net>
2023-12-24 16:56:33 +1100

    star 47

--- solver.scala
+++ solver.scala
@@ -11,84 +11,99 @@ import scala.collection.immutable.Queue
 
 import scala.collection.mutable
 
-def stepWithCondition(path:List[Coord], allowedDirections:Map[Coord, Seq[Coord]])(cond: List[Coord] => Boolean):Seq[List[Coord]] = {
-    val found = mutable.Buffer.empty[List[Coord]]
+type Coord3 = (Long, Long, Long)
 
-    // @tailrec
-    def step(path:List[Coord]):Unit = 
-        val (p :: t) = path
-        for d <- allowedDirections(p) if !path.contains(p + d) do 
-            if (cond((p + d) :: path)) then
-                found.append((p + d) :: path)
-            else 
-                step((p + d) :: path)
+type Hailstone = (Coord3, Coord3)
 
-    step(path)
-    found.toSeq
+extension (h:Hailstone) {
+
+    def x = h._1._1
+    def y = h._1._2
+    def z = h._1._3
+
+    def vx = h._2._1
+    def vy = h._2._2
+    def vz = h._2._3
+
+    def yGradient:Double = 
+        val ((x, y, z), (vx, vy, vz)) = h
+        vy.toDouble / vx
+
+    // x value where it crosses the y axis
+    def yCrossing:Double =
+        val ((x, y, z), (vx, vy, vz)) = h
+        x - y / yGradient
+
+    def y0 = h.yAt(0)
+
+    def isFutureX(x:Double) = 
+        val dir = (Math.abs(h.vx) / h.vx).toInt
+        val dir2 = (Math.abs(x - h.x) / (x - h.x)).toInt
+        dir == dir2
+
+    def yAt(x:Double) = 
+        (x - h.yCrossing) * h.yGradient
+
+    def xyIntersect(h2:Hailstone) :Option[(Double, Double)] = 
+        if h.yParallel(h2) then None else
+            val ydiff = h.y0 - h2.y0
+            val x = ydiff / (h2.yGradient - h.yGradient)
+            val y = h.yGradient * (x - h.yCrossing)
+            Some((x, y))
+
+
+    def yParallel(h2:Hailstone) = 
+        h.yGradient == h2.yGradient
+
+    // def yCrossing(h2:Hailstone):Option[(Double, Double)] = 
+    //     if h.yParallel(h2) then None else
+    //         val gDiff = h.yGradient - h2.yGradient
+    //         // 
+    //         val y0 = h2.y (h.x - h2.x) * gDiff
 
 }
+
+def decompose(line:String):Hailstone = {
+    val s"$x, $y, $z @ $vx, $vy, $vz" = line
+    val p = (x.trim.toLong, y.trim.toLong, z.trim.toLong)
+    val v = (vx.trim.toLong, vy.trim.toLong, vz.trim.toLong)
+    (p, v)
+}
+
+// Solves for t when x coordinates cross. Unfortunately, RTQ, that's not what we're doing
+def xCrossingT(h1:Hailstone, h2:Hailstone) = {
+    // x1 + tVx1 = x2 + tVx2
+    // x1 - x2 = tVx2 - tVx1
+    // x1 - x2 = t(Vx2 - Vx1)
+    // t = (x1 - x2) / (Vx2 - Vx1)
+
+    (h1._1._1 - h2._1._1).toDouble / (h2._2._1 - h1._2._1).toDouble    
+}
+
+
 
 @main def main() = 
     val lines = Source.fromFile("input.txt").getLines().toSeq
 
-    val maze:Seq[Seq[Char]] = lines.map(_.toSeq)
-    val mazeMap = (for 
-        y <- maze.indices
-        x <- maze(y).indices
-    yield (x, y) -> maze(y)(x)).toMap
+    val hailstones = lines.map(decompose)
 
-    val start = (maze(0).indexOf('.'), 0)
-    val target = (maze(maze.indices.last).indexOf('.'), maze.indices.last)
+    val pairs = hailstones.combinations(2)
 
-    val j0 = JellyFlood(mazeMap) // only because I happened to leave the map of allowable directions in there
-    val junctions = (for (p, dirs) <- j0.allowedDirections.toSeq if mazeMap(p) != '#' && dirs.length > 2 yield p -> dirs).toMap
-    val junctionPositions = junctions.keySet + start + target
-
-    println("Junctions " + junctionPositions.size)
-
-    val junctionMap = (for 
-        j <- junctionPositions.toSeq 
-        dirs = j0.allowedDirections(j)
-    yield {
-        j -> (for d <- dirs yield
-            val neighbours = stepWithCondition(List(j + d, j), j0.allowedDirections) {
-                case (p :: t) => junctionPositions.contains(p) || p == start || p == target
-            }
-            val path = neighbours(0)
-            path.head -> (path.length - 1)
-        )
-
-    }).toMap
-
-
-    // current junction, visited, length
-    type JunctionPath = (Coord, Set[Coord], Int)
-
-    val begin = List((start, 0))
-
-    def jstep(jp:JunctionPath):Seq[JunctionPath] = {
-        val (p, tail, cost) = jp
-        for (next, d) <- junctionMap(p) if !tail.contains(next) yield
-            (next, tail + p, cost + d)
-    }
-
-    // val allPaths = mutable.Buffer.empty[JunctionPath]
-    var longest:Option[Int] = None
-    var cursor:Seq[JunctionPath] = Seq((start, Set.empty, 0))
-    println("Go!")
-    while cursor.nonEmpty do 
-        println(s"Processing ${cursor.length} paths")
-        val atTarget = cursor.filter(_._1 == target)
-        if atTarget.nonEmpty then
-            val long = atTarget.map(_._3).max
-            if longest.isEmpty || longest.get < long then
-                println(s"New longest $long")
-                longest = Some(long)
-        
-        cursor = cursor.flatMap(jstep)
-
+    // val (low, high) = (7L, 27L)
+    val (low, high) = (200000000000000L, 400000000000000L)
     
-    println(s"Done, longest was $longest")
+    val result = pairs.count({ case Seq(a, b) => 
+        a.xyIntersect(b).exists((xx, yy) => 
+
+            val r = low <= xx && xx <= high && low <= yy && yy <= high && a.isFutureX(xx) && b.isFutureX(xx)
+
+            println(s"$a $b  -- $xx $yy -- $r")
+            r
+
+        )
+    })
+
+    println(s"Result is $result")
 
 
 
